42e4d546f80ba5a5e42038171f13ab187b45302f
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2022-10-10 13:02:41 +0200

    feat: send slack message on order_created event

--- pages/api/webhooks/order-created.ts
+++ pages/api/webhooks/order-created.ts
@@ -1,23 +1,55 @@
+import { SALEOR_DOMAIN_HEADER } from "@saleor/app-sdk/const";
 import {
   withRegisteredSaleorDomainHeader,
   withSaleorEventMatch,
   withWebhookSignatureVerified,
 } from "@saleor/app-sdk/middleware";
 import { withSentry } from "@sentry/nextjs";
-import type { Handler } from "retes";
+import { Handler } from "retes";
 import { toNextHandler } from "retes/adapter";
 import { Response } from "retes/response";
 
+import { getValue } from "../../../lib/metadata";
 import { apl } from "../../../lib/saleorApp";
 
 const handler: Handler = async (request) => {
-  //
-  // Your logic goes here
-  //
-  console.debug("Received order webhook.");
-  console.debug(request.body);
+  const saleorDomain = request.headers[SALEOR_DOMAIN_HEADER];
+  const webhookUrl = await getValue(saleorDomain as string, "WEBHOOK_URL");
 
-  return Response.OK({ success: true });
+  const context = request.params;
+  const {
+    order: { id, userEmail },
+  } = context;
+
+  if (!id) {
+    return Response.BadRequest({ success: false, message: "No order id." });
+  }
+
+  const response = await fetch(webhookUrl, {
+    method: "POST",
+    body: JSON.stringify({
+      blocks: [
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: `New order created for ${userEmail} 🎉\n\n<https://${saleorDomain}/dashboard/orders/${id}|View order>`,
+          },
+        },
+      ],
+    }),
+  });
+
+  if (response.status !== 200) {
+    return Response.InternalServerError({
+      success: true,
+      message: `Slack API responded with status ${
+        response.status
+      }. Message: ${await response.text()}`,
+    });
+  }
+
+  return Response.OK({ success: true, message: "Slack message sent!" });
 };
 
 export default withSentry(
