6eea08d3b5dd0d6b80abce42966ffab080524297
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2022-10-14 11:55:59 +0200

    feat: use Upstash APL in development

---
+++ env.d.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_UPSTASH_TOKEN: string;
+    NEXT_PUBLIC_UPSTASH_URL: string;
+  }
+}
--- lib/saleor-app.ts
+++ lib/saleor-app.ts
@@ -1,16 +1,21 @@
 import { SaleorApp } from "@saleor/app-sdk";
-import { FileAPL, VercelAPL } from "@saleor/app-sdk/APL";
+import { UpstashAPL, VercelAPL } from "@saleor/app-sdk/APL";
 
 const isVercel = process.env.VERCEL === "1";
 
 /**
- * For local development store auth data in the `.auth-data.json`.
+ * For local development store auth data in the Upstash (see docs/upstash.md for more).
  * For app deployment on Vercel with Saleor CLI, use vercelAPL.
  *
  * To read more about storing auth data, read the
  * [APL documentation](https://github.com/saleor/saleor-app-sdk/blob/main/docs/apl.md)
  */
-const apl = isVercel ? new VercelAPL() : new FileAPL();
+const apl = isVercel
+  ? new VercelAPL()
+  : new UpstashAPL({
+      restToken: process.env.NEXT_PUBLIC_UPSTASH_TOKEN,
+      restURL: process.env.NEXT_PUBLIC_UPSTASH_URL,
+    });
 
 export const saleorApp = new SaleorApp({
   apl,
