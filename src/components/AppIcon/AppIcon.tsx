c6e2ded409577e8b61c1bdc382608058013caba8
Krzysztof Wolski <krzysztof.wolski@saleor.io>
2023-01-27 12:37:49 +0100

    Update the icon

--- src/components/AppIcon/AppIcon.tsx
+++ src/components/AppIcon/AppIcon.tsx
@@ -1,12 +1,15 @@
+import { Typography } from "@material-ui/core";
 import { makeStyles } from "@saleor/macaw-ui";
-import Image from "next/image";
-import appIcon from "./AppIcon.svg";
 
 const useStyles = makeStyles({
   appIconContainer: {
     background: `rgb(95, 58, 199)`,
-    padding: 10,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
     borderRadius: "50%",
+    color: "#fff",
     width: 50,
     height: 50,
   },
@@ -17,7 +20,9 @@ export const AppIcon = () => {
 
   return (
     <div className={styles.appIconContainer}>
-      <Image width={30} height={30} alt="icon" src={appIcon} />
+      <div>
+        <Typography variant="h2">S</Typography>
+      </div>
     </div>
   );
 };
